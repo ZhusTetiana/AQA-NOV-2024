c91105aacd44e122e9e3d17854e49f4ff27761fc
user <timontaniaroma@gmail.com>
2024-12-24 16:33:10 +0200

    Home_work_8

--- src/main/java/org/prog/selenium/ActionsDemo.java
+++ src/main/java/org/prog/selenium/ActionsDemo.java
@@ -22,14 +22,37 @@ public class ActionsDemo {
 
             List<WebElement> searchResults =
                     new WebDriverWait(driver, Duration.ofSeconds(60))
-                            .until(ExpectedConditions.numberOfElementsToBeMoreThan(By.className("products-layout__item"), 10));
+                            .until(ExpectedConditions.numberOfElementsToBeMoreThan(By.className("product-sku__value"), 10));
 
             Actions actions = new Actions(driver);
-            actions.moveToElement(searchResults.get(0));
-            actions.perform();
+            WebElement firstElement = searchResults.get(0);
+
+            String codeOfItemOfFirstElement = firstElement.getText();
+
+            try {
+
+                int value = searchResults.indexOf(searchResults.get(0)) + 1;
+
+                if (value < 0) {
+                    throw new RuntimeException("Less than 0!");
+                }
+
+                if (value > searchResults.size()) {
+                    throw new RuntimeException("First item more than entire list");
+                }
+
+                if (0 <= value && value <= searchResults.size()) {
+                    actions.moveToElement(firstElement).perform();
+                    System.out.println("Код первой позиции: "+codeOfItemOfFirstElement);
+                }
+
+            } catch (NumberFormatException e) {
+                System.out.println("Не удалось преобразовать текст в число");
+            }
 
             JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
             javascriptExecutor.executeScript("alert('tests!!')");
+
 
             System.out.println("look!");
         } finally {
