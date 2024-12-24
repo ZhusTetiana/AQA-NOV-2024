d5e92ae5d6913d74e3d9e1992b4002d4bc8f45eb
user <timontaniaroma@gmail.com>
2024-12-24 19:16:46 +0200

    Home_work_8.1TestNG

--- src/main/java/org/prog/selenium/pages/AlloUaPage.java
+++ src/main/java/org/prog/selenium/pages/AlloUaPage.java
@@ -1,7 +1,62 @@
 package org.prog.selenium.pages;
-// add method which checks goods id on position X
-// if X < 0 -> throw exception
-// if X > goods count -> throw exception
-// if 0 <= X <= goods_amount -> scroll to item, then print goods id for that item
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 public class AlloUaPage {
+
+    private final WebDriver driver;
+
+    public AlloUaPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void loadPageAndAcceptCookiesIfPresent() {
+        loadPage();
+        if (isCookiePresent()) {
+            acceptCookies();
+        }
+    }
+    public boolean isCookiePresent() {
+        List<WebElement> cookiesLink =
+                driver.findElements(By.xpath("//a[contains(@href, 'technologies/cookies')]"));
+        if (!cookiesLink.isEmpty()) {
+            return cookiesLink.get(0).isDisplayed();
+        }
+        return false;
+    }
+
+    public List<WebElement> searchResult() {
+        return new WebDriverWait(driver, Duration.ofSeconds(60))
+                .until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.className("product-sku__value")));
+    }
+
+    public void acceptCookies() {
+        driver.findElements(By.tagName("button")).get(4).click();
+    }
+
+    public void loadPage() {
+        driver.get("https://allo.ua/");
+    }
+
+
+    public void setSearchInputText(String value) {
+        driver.findElement(By.id("search-form__input")).sendKeys(value);
+    }
+
+    public void executeSearch() {
+        driver.findElement(By.id("search-form__input")).sendKeys(Keys.ENTER);
+    }
+
+
+
+    public List<WebElement> getSearchHeaders() {
+        return new WebDriverWait(driver, Duration.ofSeconds(5L))
+                .until(ExpectedConditions.numberOfElementsToBeMoreThan(By.tagName("h3"), 1));
+    }
 }
--- src/test/java/org/prog/testng/AlloUaTestNG.java
+++ src/test/java/org/prog/testng/AlloUaTestNG.java
@@ -1,6 +1,69 @@
 package org.prog.testng;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.prog.selenium.pages.AlloUaPage;
+import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import java.util.List;
 
 //TODO: Move Allo.ua code to TestNG
 //TODO: replace [// if X > goods count -> throw exception] with Assert
 public class AlloUaTestNG {
+
+    private WebDriver driver;
+    private AlloUaPage alloPage;
+    @BeforeSuite
+    public void setUp() {
+        driver = new ChromeDriver();
+        alloPage = new AlloUaPage(driver);
+
+    }
+
+    @AfterSuite
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test(dataProvider = "phoneNames")
+    public void testGoogleSearchClassic(String phoneNames) {
+        alloPage.loadPageAndAcceptCookiesIfPresent();
+        alloPage.setSearchInputText(phoneNames);
+        alloPage.executeSearch();
+        List<WebElement> searchResults = alloPage.searchResult();
+        WebElement firstElement = searchResults.get(0);
+
+        String codeOfItemOfFirstElement = firstElement.getText();
+
+        try {
+            int value = alloPage.searchResult().indexOf(searchResults.get(0)) + 1;
+            if (value < 0) {
+                throw new RuntimeException("Less than 0!");
+            }
+
+            Assert.assertFalse(value > searchResults.size(),
+                        "First item more than entire list");
+
+            if (0 <= value && value <= searchResults.size()) {
+
+                System.out.println("Код первой позиции: "+codeOfItemOfFirstElement);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Не удалось преобразовать текст в число");
+        }
+    }
+
+    @DataProvider
+    public Object[][] phoneNames() {
+        return new Object[][]{
+                {"iphone"},
+
+        };
+    }
+
 }
